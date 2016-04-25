65f08f8c1f5a33a3d4cd632dd635e2e1f729ad45
BrianPhiri <brianphiri.9523@gmail.com>
2016-04-25 14:35:55 +0300

    Enter listening commands

--- PersonnalAssistant/Form1.cs
+++ PersonnalAssistant/Form1.cs
@@ -7,14 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
 
 namespace PersonnalAssistant
 {
     public partial class Form1 : Form
     {
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        SpeechSynthesizer spk = new SpeechSynthesizer();
+        Choices list = new Choices();
         public Form1()
         {
+            letsTalk();
+            introduction();
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void introduction(){
+            spk.Speak("Hello, how may I help you?");
+        }
+
+        public void letsTalk() {
+            //list of key words
+            list.Add(new string[] { "hello", "hey" });
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(gr);
+                sre.SpeechRecognized += sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            catch { return; }
+
+        }
+
+        private void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string command = e.Result.Text;
+
+            if(command == "hey")
+            {
+                say("I am working");
+            }
+
+        }
+
+        //her reply
+        public void say(string res)
+        {
+            spk.Speak(res);
+        }
+
     }
 }
