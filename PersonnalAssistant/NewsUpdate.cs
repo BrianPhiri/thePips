318edce2d38336922b7e97769fc5f6113f9b3bc5
BrianPhiri <brianphiri.9523@gmail.com>
2016-04-25 21:57:52 +0300

    trying to pull data from website

---
+++ PersonnalAssistant/NewsUpdate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PersonnalAssistant
+{
+    class NewsUpdate : Form1
+    {
+        public void news() {
+            List<string> headlines = new List<string>();
+
+            WebClient wc = new WebClient();
+            String html = wc.DownloadString("http://www.nation.co.ke/");
+            MatchCollection m1 = Regex.Matches(html, @"</h1><p>\s(.+?)\s</p><figure>", RegexOptions.Singleline);
+
+            foreach(Match m in m1)
+            {
+                
+                string headline = m.Groups[1].Value;
+                headlines.Add(headline);
+
+                //say(headline);
+               Console.WriteLine(headline);
+            }
+           
+        }
+    }
+}
