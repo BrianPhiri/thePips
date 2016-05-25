1e2e057e8171bbcdfa229a7fb39dcb64e56ee511
Kareithi Lucy <Kareithi Lucy>
2016-05-25 11:54:12 +0300

    ...

--- app/Products.php
+++ app/Products.php
@@ -3,13 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletingTrait;
+// use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
 
 class Products extends Model
 {
     static $products = 'products';
-    protected $dates = ['deleted_at'];
+    // protected $dates = ['deleted_at'];
     protected $fillable = [
       'name', 'sub_category_id','price', 'description', 'image'
     ];
