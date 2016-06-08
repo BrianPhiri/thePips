47f9023ee5353b24288939157734ef9e0f19f227
Nickson Munge <nixmunge@gmail.com>
2016-06-08 13:47:20 +0000

    cart

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'The Pips',
+            'title_id' => 1,
+            'email' => 'test@pips.com',
+            'password' => bcrypt('login'),
+            'verified' => 1,
+        ]);
+    }
+}
