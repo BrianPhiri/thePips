0a20c8669553df2a95de7de0018283b55c856704
Brian Phiri <Brian Phiri>
2016-06-10 10:04:02 +0300

    Cart For real now

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Request;
+// use Illuminate\Http\Request;
 use App\CartItem;
-// use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Database\Eloquent\Model;
 use App\ShopCart;
 use App\Products;
@@ -18,52 +18,129 @@ use Cart;
 
 class CartController extends Controller
 {
-  public function cart() {
-    //update/ add new item to cart
-    if (Request::isMethod('post')) {
-        $product_id = Request::get('product_id');
-        $product = Products::find($product_id);
-        Cart::add(array('id' => $product_id, 'name' => $product->name, 'qty' => 1, 'price' => $product->price));
-        if(!Auth::check()){
-            //
-          }else{
-            $cartItems = Cart::content();
-            foreach($cartItems as $cartItem){
-              $cartPost = new ShopCart();
-              $fromShopCart = ShopCart::where('product_id', '=', $cartItem->id)->select('product_id', 'user_id')->get();
-              $cartPost->user_id=Auth::user()->id;
-              $cartPost->product_id=$cartItem->id;
-              $cartPost->quantity=$cartItem->qty;
-              $cartPost->save();
-          }
-        }
+  public function cart()
+  {
+    // remove items from the Cart
+    if(Request::get('yes'))
+    {
+      Cart::destroy();
+      Redirect::back();
     }
+    // Special Increments and Decrements for the database.
+    // if (Request::get('product_id') && (Request::get('increment')) == 1 && Request::get('yes'))
+    // {
+    //   // For the database
+    //   $dbRowId = ShopCart::where('product_id', Request::get('product_id'))->firstOrFail()->quantity;
+    //   // Increment the quantity by 1.
+    //   $newQuantity = $dbRowId + 1;
+    //   // Update the database
+    //   ShopCart::where('product_id', Request::get('product_id'))->update(['quantity' => $newQuantity]);
+    // }
+    // elseif (Request::get('product_id') && (Request::get('decrease')) == 1 && Request::get('yes'))
+    // {
+    //    // For the database
+    //   $dbRowId = ShopCart::where('product_id', Request::get('product_id'))->first()->quantity;
+    //   // Decrement the quantity by 1.
+    //   $newQuantity = $dbRowId - 1;
+    //   // Update the database
+    //   ShopCart::where('product_id', Request::get('product_id'))->update(['quantity' => $newQuantity]);
+    // }
+    // else
+    // {
+    //   // remove an item from the cart
+    //   if(Request::get('product_id') && (Request::get('remove')) && Request::get('yes'))
+    //   {
+    //      // For the database
+    //       $dbRowId = ShopCart::where('product_id', Request::get('product_id'))->first()->delete();
+    //   }
+    // }
+    // Not fully retrievable.
     //increment the quantity
     if (Request::get('product_id') && (Request::get('increment')) == 1) {
-        $rowId = Cart::search(array('id' => Request::get('product_id')));
-        $item = Cart::get($rowId[0]);
-
-        Cart::update($rowId[0], $item->qty + 1);
+      $rowId = Cart::search(array('id' => Request::get('product_id')));
+      $item = Cart::get($rowId[0]);
+      Cart::update($rowId[0], $item->qty + 1);
+      // For the database
+      $dbRowId = ShopCart::where('product_id', Request::get('product_id'))->first()->quantity;
+      // Increment the quantity by 1.
+      $newQuantity = $dbRowId + 1;
+      // Update the database
+      ShopCart::where('product_id', Request::get('product_id'))->update(['quantity' => $newQuantity]);
+      // return Redirect::back();
     }
 
     //decrease the quantity
-    if (Request::get('product_id') && (Request::get('decrease')) == 1) {
+    elseif (Request::get('product_id') && (Request::get('decrease')) == 1) {
+      $rowId = Cart::search(array('id' => Request::get('product_id')));
+      $item = Cart::get($rowId[0]);
+      Cart::update($rowId[0], $item->qty - 1);
+      // For the database
+      $dbRowId = ShopCart::where('product_id', Request::get('product_id'))->first()->quantity;
+      // Decrement the quantity by 1.
+      $newQuantity = $dbRowId - 1;
+      // Update the database
+      ShopCart::where('product_id', Request::get('product_id'))->update(['quantity' => $newQuantity]);
+      // return Redirect::back();
+    }
+    else
+    {
+      // remove an item from the cart
+      if(Request::get('product_id') && (Request::get('remove')))
+      {
         $rowId = Cart::search(array('id' => Request::get('product_id')));
-        $item = Cart::get($rowId[0]);
+        Cart::remove($rowId[0]);
+      }
 
-        Cart::update($rowId[0], $item->qty - 1);
     }
 
-    if(!Auth::check()){
+    if (!Auth::check())
+    {
+      // Stores the items in the shop cart temporarily during a session.
+      if (Request::isMethod('post')) {
+        $product_id = Request::get('product_id');
+        $product = Products::find($product_id);
+        Cart::add(array('id' => $product_id, 'name' => $product->name, 'qty' => 1, 'price' => $product->price, 'options' => ['image' => $product->image]));
+      }
+
       $cart = Cart::content();
-      return view('homepage.cart', array('cart' => $cart, 'title' => 'Welcome', 'description' => '', 'page' => 'home'));
-    }else if(Auth::check()){
-      // $cart = ShopCart::all();
-      $cart = ShopCart::with('products')->get();
-      return view('homepage.cart', array('cart' => $cart, 'title' => 'Welcome', 'description' => '', 'page' => 'home'));
+      // return $cart;
+      return view('homepage.cart', array('db' => null, 'cart' => $cart, 'title' => 'Welcome', 'description' => '', 'page' => 'home'));
     }
-    // return view('homepage.cart', array('cart' => $cart, 'title' => 'Welcome', 'description' => '', 'page' => 'home'));
+    // This is strictly for logged in persons.
+    else
+    {
+      // Stores the items in the shop cart temporarily during a session. [Even if I am logged in]
+      if (Request::isMethod('post'))
+      {
+        $product_id = Request::get('product_id');
+        $product = Products::find($product_id);
+        Cart::add(array('id' => $product_id, 'name' => $product->name, 'qty' => 1, 'price' => $product->price, 'options' => ['image' => $product->image]));
 
+        // Avoid multi-product creation
+        $cartItems = Cart::content();
+        // Should insert items into the cart table based on the user.
+        foreach ($cartItems as $kat)
+        {
+          // Items are inserted into the cart's database.
+          $cartItem =  new ShopCart();
+          $cartItem->user_id=Auth::user()->id;
+          $cartItem->product_id=$kat->id;
+          $cartItem->quantity=$kat->qty;
+          $cartItem->save();
+        }
+    }
+        $db = ShopCart::with('products')->get();
+        return view('homepage.cart', array('db' => $db, 'cart' => null, 'title' => 'Welcome', 'description' => '', 'page' => 'home'));
+     // return Products::findOrFail('shopCart')->get();
+    // $product = Products::find($dbItem->product_id);
   }
-
+}
+  /**
+  * Empty the cart.
+  */
+  public function remove()
+  {
+    Cart::destroy();
+    return Redirect::back();
+  }
 }
--- resources/views/homepage/cart.blade.php
+++ resources/views/homepage/cart.blade.php
@@ -11,6 +11,55 @@
 			<li><a href="/">Home</a></li>
 			<li class="active">Cart</li>
 		</ol>
+
+		<!-- Start -->
+		@if(count($db))
+		<div class="cart-items">
+			<h2>My Shopping Bag ({{Cart::count()}})</h2>
+			<script>$(document).ready(function(c) {
+				$('.close1').on('click', function(c){
+					$('.cart-header').fadeOut('slow', function(c){
+						$('.cart-header').remove();
+					});
+				});
+			});
+			</script>
+			@foreach($db as $item)
+			@foreach($item->products as $products)
+			<div class="cart-header">
+				<div class="close1"> </div>
+				<div class="cart-sec">
+				<!--  -->
+
+				<!--  -->
+					<div class="cart-item cyc">
+						<img src="{{URL::asset('image_uploads')}}/{{$products->image}}"/>
+					</div>
+					<div class="cart-item-info">
+						<h3>{{$products->name}}<span>Model No: {{$products->id}}</span></h3>
+						<h4><span>Ksh </span>{{$products->price}}</h4>
+						<p class="qty">Qty ::</p>
+						<form class="form-inline">
+							<a class="cart_quantity_up" href='{{url("carts?product_id=$item->product_id&increment=1")}}'><i class="fa fa-plus-square" aria-hidden="true"></i></a>
+							<input min="1" type="text" id="quantity" name="quantity" value="{{$item->quantity}}" class="form-control input-small">
+							<a class="cart_quantity_down" href='{{url("carts?product_id=$item->product_id&decrease=1")}}'><i class="fa fa-minus-square" aria-hidden="true"></i></a>
+						</form>
+						<br>
+						<a class="cpns" href='{{url("carts?product_id=$item->product_id&remove=1")}}'>Remove Item</a>
+					</div>
+					<div class="clearfix"></div>
+					<div class="delivery">
+						<p><h4>Sub-Total:: {{$item->subtotal}}</h4></p>
+					</div>
+				</div>
+			</div>
+			@endforeach
+			@endforeach
+			@else
+			<p>You have no items in the persisted shopping cart</p>
+			@endif
+		<!-- End -->
+
 		@if(count($cart))
 		<div class="cart-items">
 			<h2>My Shopping Bag ({{Cart::count()}})</h2>
@@ -22,33 +71,31 @@
 				});
 			});
 			</script>
-			@foreach($cart as $cartItems)
-			@foreach($cartItems->products as $item)
+			@foreach($cart as $item)
 			<div class="cart-header">
 				<div class="close1"> </div>
 				<div class="cart-sec">
 					<div class="cart-item cyc">
-						<img src="{{URL::asset('image_uploads')}}/{{$item->image}}"/>
+						<img src="{{URL::asset('image_uploads')}}/{{$item->options->image}}"/>
 					</div>
 					<div class="cart-item-info">
 						<h3>{{$item->name}}<span>Model No: {{$item->id}}</span></h3>
 						<h4><span>Ksh </span>{{$item->price}}</h4>
 						<p class="qty">Qty ::</p>
 						<form class="form-inline">
-							<a class="cart_quantity_up" href='{{url("cart?product_id=$item->id&increment=1")}}'> + </a>
-							<input min="1" type="text" id="quantity" name="quantity" value="{{$item->quantity}}" class="form-control input-small" readonly>
-							<a class="cart_quantity_down" href='{{url("cart?product_id=$item->id&decrease=1")}}'> - </a>
+							<a class="cart_quantity_up" href='{{url("carts?product_id=$item->id&increment=1")}}'><i class="fa fa-plus-square" aria-hidden="true"></i></a>
+							<input min="1" type="text" id="quantity" name="quantity" value="{{$item->qty}}" class="form-control input-small">
+							<a class="cart_quantity_down" href='{{url("carts?product_id=$item->id&decrease=1")}}'><i class="fa fa-minus-square" aria-hidden="true"></i></a>
 						</form>
 						<br>
 						<a class="cpns" href='{{url("carts?product_id=$item->id&remove=1")}}'>Remove Item</a>
 					</div>
 					<div class="clearfix"></div>
 					<div class="delivery">
-						<!-- <p><h4>Sub-Total:: {{$item->subtotal}}</h4></p> -->
+						<p><h4>Sub-Total:: {{$item->subtotal}}</h4></p>
 					</div>
 				</div>
 			</div>
-			@endforeach
 			@endforeach
 			@else
 			<p>You have no items in the session shopping cart</p>
@@ -68,7 +115,7 @@
 			<div class="price-details">
 				<h3>Price Details</h3>
 				<span>Total</span>
-				<span class="total">Ksh. </span>
+				<span class="total">Ksh. {{Cart::total()}}</span>
 				<span>Discount</span>
 				<span class="total">---</span>
 				<!-- <span>Delivery Charges</span> -->
@@ -78,28 +125,16 @@
 			<h4 class="last-price">TOTAL</h4>
 			<span class="total final">{{Cart::total()}}</span>
 			<div class="clearfix"></div>
-			<form action="{{URL::asset('checkout')}}" method="POST">
-				<input type="hidden" name="_token" value="{{ csrf_token() }}">
-			<script
-				src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-				data-key="pk_test_GIjjWsmv1wDgFvt2BG0Fhtpl"
-				data-amount="{{Cart::total()}} *100"
-				data-name="ThePips"
-				data-description="Buying product"
-				data-image="/img/documentation/checkout/marketplace.png"
-				data-locale="auto">
-			</script>
-			</form>
+			<a class="order" href="{{url("carts/")}}">Place Order</a>
 			<div class="total-item">
 				<h3>OPTIONS</h3>
 				<h4>COUPONS</h4>
 				<a class="cpns" href="#">Apply Coupons</a>
-				<!-- <p><a href="#">Log In</a> to use accounts - linked coupons</p> -->
+				<p><a href="#">Log In</a> to use accounts - linked coupons</p>
 			</div>
 		</div>
 	</div>
 </div>
 <!---->
-
 @include('homepage._footer')
 @endsection
