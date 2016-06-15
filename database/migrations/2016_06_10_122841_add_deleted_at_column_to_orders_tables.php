eb1dfe2c8cdb1827924e08dc876399b7c92bd324
Brian Mwathi <mwathibrian7@gmail.com>
2016-06-15 16:50:41 +0300

    Sidebar

--- database/migrations/2016_06_10_122841_add_deleted_at_column_to_orders_tables.php
+++ database/migrations/2016_06_10_122841_add_deleted_at_column_to_orders_tables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDeletedAtColumnToOrdesTables extends Migration
+class AddDeletedAtColumnToOrdersTables extends Migration
 {
     /**
      * Run the migrations.
