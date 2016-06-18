a5f82c6da8f2ece3ff4c27c00b777c6785ce8723
BrianPhiri <brianphiri.9523@gmail.com>
2016-06-18 09:14:40 +0300

    edit in products

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -15,12 +15,10 @@
 
  		<div class="validation-form">
  	<!---->
-
+			 This product belongs the sub category: {{ $subcategory_name->name }}
 			{!! Form::model($product,  ['method' => 'PATCH', 'url' => 'product/'. $product->id ]) !!}
 				@include('admin.products._form', ['submitButtonText' => 'Update Product'])
 			{!! Form::close() !!}
 
  	<!---->
-
-
 @endsection
