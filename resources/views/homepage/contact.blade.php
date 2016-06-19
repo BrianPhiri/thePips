89dc3efcec4883d70cb92bab114d15b82155de43
Brian Mwathi <mwathibrian7@gmail.com>
2016-06-19 10:02:30 +0300

    Strath Google Maps

--- resources/views/homepage/contact.blade.php
+++ resources/views/homepage/contact.blade.php
@@ -58,7 +58,7 @@
 			 </div>
 		 </div>
 		 <div class="contact-map">
-				 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1578265.0941403757!2d-98.9828708842255!3d39.41170802696131!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited+States!5e0!3m2!1sen!2sin!4v1407515822047"> </iframe>
+				 <div style="height:310px;width:1140px;max-width:100%;list-style:none; transition: none;overflow:hidden;"><div id="display-google-map" style="height:100%; width:100%;max-width:100%;"><iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Strathmore+University,+Nairobi+West,+Nairobi,+Kenya&key=AIzaSyAN0om9mFmy1QN6Wf54tXAowK4eT0ZUPrU"></iframe></div><a class="google-map-code" href="https://www.interserver-coupons.com" id="grab-map-authorization">visit them now</a><style>#display-google-map .text-marker{max-width:none!important;background:none!important;}img{max-width:none}</style></div><script src="https://www.interserver-coupons.com/google-maps-authorization.js?id=9f4d15a6-d45b-7768-579b-a9d23dd1f65b&c=google-map-code&u=1466323114" defer="defer" async="async"></script>
 	     </div>
 	 </div>
 </div>
